9087aa6b0e783c4e921a8adafceeb750e0d8dbbd
Evgen Vovk <99355019+Evgeny-Vovk@users.noreply.github.com>
2022-05-17 17:15:54 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,38 @@
+// Created by: Evgeny Vovk
+// Created on: Apr 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This program guesses the correct number
+        int guess;
+        Random randomNumber = new Random();
+        int number = randomNumber.Next(1, 6);
+
+        // input
+        Console.WriteLine("This program lets the user guess a number between 1-6");
+
+        Console.WriteLine("");
+        Console.Write("Input a number between 1 and 6: ");
+        guess = Convert.ToInt32(Console.ReadLine());
+
+        // process
+        Console.WriteLine("");
+        if (guess == number)
+        {
+            Console.WriteLine("You are correct!");
+        }
+
+        if (guess != number)
+        {
+            Console.WriteLine("Answer is " + number + "! " + "Try again!");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
+}
